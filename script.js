af669cbf7d0e48c53be63fd2012cf36befcdcdee
Will G <wgrout87@gmail.com>
2022-03-28 22:51:17 -0600

    assign random position to selected character options

--- script.js
+++ script.js
@@ -1,39 +1,66 @@
 var passwordLength = 8;
 var lowerCase = {
 	chars: "abcdefghijklmnopqrstuvwxyz",
-	wasSelected: false
+	wasSelected: true,
+	position: null
 };
 var upperCase = {
 	chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-	wasSelected: false
+	wasSelected: true,
+	position: null
 };
 var numeric = {
 	chars: "0123456789",
-	wasSelected: false
+	wasSelected: true,
+	position: null
 };
 var specialCharacter = {
 	chars: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
-	wasSelected: false
+	wasSelected: true,
+	position: null
 };
 var passwordCharacters = "";
+var positionArray = [];
 
 var chosenCharacters = function () {
+	randomPositon();
 	if (lowerCase.wasSelected) {
 		passwordCharacters += lowerCase.chars;
+		lowerCase.position = positionArray[0];
+		console.log(lowerCase.position);
 	};
 	if (upperCase.wasSelected) {
 		passwordCharacters += upperCase.chars;
+		upperCase.position = positionArray[1];
+		console.log(upperCase.position);
 	};
 	if (numeric.wasSelected) {
 		passwordCharacters += numeric.chars;
+		numeric.position = positionArray[2];
+		console.log(numeric.position);
 	};
 	if (specialCharacter.wasSelected) {
 		passwordCharacters += specialCharacter.chars;
+		specialCharacter.position = positionArray[3];
+		console.log(specialCharacter.position);
 	};
 	console.log(passwordCharacters);
 };
 
-chosenCharacters();
+var randomPositon = function () {
+	while (positionArray.length < 4) {
+		var possiblePosition = Math.floor(Math.random() * passwordLength);
+		if (positionArray.indexOf(possiblePosition) === -1) {
+			positionArray.push(possiblePosition);
+		}
+	}
+	console.log(positionArray);
+};
+
+var generatePassword = function() {
+	chosenCharacters();
+
+};
 
 
 // Get references to the #generate element
