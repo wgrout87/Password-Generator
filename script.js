748125ae286c2eac91b4e3f1e1dc4e54426bc8b3
Will G <wgrout87@gmail.com>
2022-03-29 09:59:13 -0600

    user prompts with validation

--- script.js
+++ script.js
@@ -6,28 +6,31 @@ var lowerCase = {
 	// String contains all possible options for lower case letters
 	chars: "abcdefghijklmnopqrstuvwxyz",
 	// Boolean value is changed to true if the user selects lower case letter in their password
-	wasSelected: true,
+	wasSelected: false,
 	// The position value is given a value in the chosenCharacters() function and referenced for password generation
 	position: null
 };
+
 var upperCase = {
 	chars: "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
 	// Boolean value is changed to true if the user selects upper case letter in their password
-	wasSelected: true,
+	wasSelected: false,
 	// The position value is given a value in the chosenCharacters() function and referenced for password generation
 	position: null
 };
+
 var numeric = {
 	chars: "0123456789",
 	// Boolean value is changed to true if the user selects numeric values in their password
-	wasSelected: true,
+	wasSelected: false,
 	// The position value is given a value in the chosenCharacters() function and referenced for password generation
 	position: null
 };
+
 var specialCharacter = {
 	chars: " !\"#$%&'()*+,-./:;<=>?@[\\]^_`{|}~",
 	// Boolean value is changed to true if the user selects special characters in their password
-	wasSelected: true,
+	wasSelected: false,
 	// The position value is given a value in the chosenCharacters() function and referenced for password generation
 	position: null
 };
@@ -48,7 +51,6 @@ var randomPositon = function () {
 			positionArray.push(possiblePosition);
 		}
 	}
-	console.log(positionArray);
 };
 
 // This function builds up the passwordCharacters string with additional character options and sets the position for any of the character option objects so they can be definitively included in the generated password
@@ -61,35 +63,37 @@ var chosenCharacters = function () {
 		passwordCharacters += lowerCase.chars;
 		// The position is set in the lowerCase object
 		lowerCase.position = positionArray[0];
-		console.log(lowerCase.position);
 	};
 	if (upperCase.wasSelected) {
 		// Upper case are added to the passwordCharacters string
 		passwordCharacters += upperCase.chars;
 		// The position is set in the upperCase object
 		upperCase.position = positionArray[1];
-		console.log(upperCase.position);
 	};
 	if (numeric.wasSelected) {
 		// numerical values are added to the passwordCharacters string
 		passwordCharacters += numeric.chars;
 		// The position is set in the numeric object
 		numeric.position = positionArray[2];
-		console.log(numeric.position);
 	};
 	if (specialCharacter.wasSelected) {
 		// Special characters are added to the passwordCharacters string
 		passwordCharacters += specialCharacter.chars;
 		// The position is set in the specialCharacters object
 		specialCharacter.position = positionArray[3];
-		console.log(specialCharacter.position);
 	};
 	// END CHECKING FOR OPTIONS SELECTED
-	console.log(passwordCharacters);
 };
 
 // This function generates the new password, making sure to include at least one of each selected character type
 var generatePassword = function() {
+	// BEGIN CALL TO RUN USER PROMPTS
+	// Length is prompted and checked
+	lengthPrompt();
+
+	// Character option prompts are called and checked
+	characterOptionPrompts();
+
 	// chosenCharacters() is called to establish what characters will go into the password and where
 	chosenCharacters();
 	// A local variable is set to an empty string. This will become the new password
@@ -110,6 +114,7 @@ var generatePassword = function() {
 		newPassword += passwordCharacters.charAt(Math.floor(Math.random() * passwordCharacters.length));
 		}
 	}
+	console.log(newPassword.length);
 
 	// Resets values for next password generation
 	valuesReset();
@@ -131,8 +136,81 @@ var valuesReset = function () {
 	specialCharacter.position = null;
 	passwordCharacters = "";
 	positionArray = [];
-	console.log(passwordCharacters);
-	console.log(positionArray);
+};
+
+// This function will prompt the user for password length
+var lengthPrompt = function () {
+	console.log("the user is being prompted for length");
+	var chosenLength = window.prompt("Please choose the length of your password from 8 to 128 characters.");
+	// User input is parsed to an integer value
+	chosenLength = parseInt(chosenLength);
+	// User input is validated
+	if (chosenLength >= 8 && chosenLength <= 128) {
+		passwordLength = chosenLength;
+	}
+	// Invalid input alerts the user and calls for the prompt again
+	else {
+		window.alert("Please choose a length by entering a whole number value from 8 to 128.");
+		lengthPrompt();
+	}
+	console.log(passwordLength);
+};
+
+// BEGIN CHARACTER SET OPTION PROMPT FUNCTIONS
+// This function will ask the user if they would like to include lower case characters
+var lowerCaseConfirm = function () {
+	console.log("the user is being prompted for the use of lower case");
+	// useLowerCase is set to the boolean value returned by the user input
+	var useLowerCase = window.confirm("Would you like to include lowercase in your new password?\nSelect \"OK\" to include lowercase, or \"Cancel\" to exclude lowercase.");
+	// The lowerCase object's wasSelected property is updated
+	lowerCase.wasSelected = useLowerCase;
+	console.log(lowerCase.wasSelected);
+};
+
+// This function will ask the user if they would like to include upper case characters
+var upperCaseConfirm = function () {
+	console.log("the user is being prompted for the use of upper case");
+	// useUpperCase is set to the boolean value returned by the user input
+	var useUpperCase = window.confirm("Would you like to include uppercase in your new password?\nSelect \"OK\" to include uppercase, or \"Cancel\" to exclude uppercase.");
+	// The upperCase object's wasSelected property is updated
+	upperCase.wasSelected = useUpperCase;
+	console.log(upperCase.wasSelected);
+};
+
+// This function will ask the user if they would like to include numeric characters
+var numericConfirm = function () {
+	console.log("the user is being prompted for the use of numeric characters");
+	// useNumeric is set to the boolean value returned by the user input
+	var useNumeric = window.confirm("Would you like to include numeric characters in your new password?\nSelect \"OK\" to include numeric characters, or \"Cancel\" to exclude numeric characters.");
+	// The numeric object's wasSelected property is updated
+	numeric.wasSelected = useNumeric;
+	console.log(numeric.wasSelected);
+};
+
+// This function will ask the user if they would like to include special characters
+var specialCharacterConfirm = function () {
+	console.log("the user is being prompted for the use of special characters");
+	// useSpecialCharacters is set to the boolean value returned by the user input
+	var useSpecialCharacters = window.confirm("Would you like to include special characters in your new password?\nSelect \"OK\" to include special characters, or \"Cancel\" to exclude special characters.");
+	// The specialCharacter object's wasSelected property is updated
+	specialCharacter.wasSelected = useSpecialCharacters;
+	console.log(specialCharacter.wasSelected);
+};
+// END CHARACTER SET OPTION PROMPT FUNCTIONS
+
+// Runs the functions that will prompt for character set selection and validates that at least one character set was chosen
+var characterOptionPrompts = function () {
+	// These functions will prompt the user to choose what characters to include in their password generation
+	lowerCaseConfirm();
+	upperCaseConfirm();
+	numericConfirm();
+	specialCharacterConfirm();
+
+	// User choices are validated to ensure at least one set of characters was chosen.
+	if (!lowerCase.wasSelected && !upperCase.wasSelected && !numeric.wasSelected && !specialCharacter.wasSelected) {
+		window.alert("Please choose at lease one set of characters for password generation.");
+		characterOptionPrompts();
+	}
 };
 
 // Get references to the #generate element
